e07818f1d20e7525bdb5678c14c88b4de20f7c93
Emily-Robeson <emily.robeson@gmail.com>
2023-09-15 16:36:05 +0100

    ver 1.3

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const media = ['The Orville', 'Futurama', 'Wheel of Time', 'TDP', 'Taskmaster New Zealand', 'ATLA', "Marvel thing we're behind on", "YouTube stuff (Em's choice)", "YouTube stuff (El's choice)", "Movie from Em's list", "Movie from El's list", "Show from Em's list", "Show from El's list"];
 
 const random = Math.floor(Math.random() * media.length);
-console.log(random, media[random]);
+console.log(random,media[random]);
+document.write(random, media[random]);
 
 let resultDiv = `"Today's thing to watch is: " + ${random} + '.'` 
 
